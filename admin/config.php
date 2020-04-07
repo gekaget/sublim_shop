56f3340e5f490cf154d9d35057aa33fdd58c919d
gekaget83 <igetmanets83@gmail.com>
2020-04-07 15:11:11 +0300

    First commit - create project.

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sublimoc.com/admin/');
+define('HTTP_CATALOG', 'http://sublimoc.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sublimoc.com/admin/');
+define('HTTPS_CATALOG', 'http://sublimoc.com/');
+
+// DIR
+define('DIR_APPLICATION', 'W:/domains/sublimoc.com/admin/');
+define('DIR_SYSTEM', 'W:/domains/sublimoc.com/system/');
+define('DIR_IMAGE', 'W:/domains/sublimoc.com/image/');
+define('DIR_LANGUAGE', 'W:/domains/sublimoc.com/admin/language/');
+define('DIR_TEMPLATE', 'W:/domains/sublimoc.com/admin/view/template/');
+define('DIR_CONFIG', 'W:/domains/sublimoc.com/system/config/');
+define('DIR_CACHE', 'W:/domains/sublimoc.com/system/storage/cache/');
+define('DIR_DOWNLOAD', 'W:/domains/sublimoc.com/system/storage/download/');
+define('DIR_LOGS', 'W:/domains/sublimoc.com/system/storage/logs/');
+define('DIR_MODIFICATION', 'W:/domains/sublimoc.com/system/storage/modification/');
+define('DIR_UPLOAD', 'W:/domains/sublimoc.com/system/storage/upload/');
+define('DIR_CATALOG', 'W:/domains/sublimoc.com/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sublimoc_bd');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
